1c14570afa7d284d1ec394f2cb5e1ad97191340f
Upland1 <107065818+Upland1@users.noreply.github.com>
2025-07-23 22:14:05 -0600

    Add routes and Protected Routes function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,60 @@
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './dashboards/HomePage';
 import MainUserDashboard from './dashboards/MainUserDashboard';
+import InvestorsDashboard from './dashboards/investors-dashboard/investorsDashboard';
+import EntrepreneurDashboard from './dashboards/entrepreneurs-dashboard/entrepreneursDashboard';
+import ProtectedRoute from './auth/protectedRoute';
 
 function App() {
+  // const [user, setUser] = useState(null); // Replace with real auth logic
+
+  const [ user ]  = useState({ 
+    id: '123',
+    name: 'John Doe',
+    role: 'entrepreneur', // or 'investor'
+
+  })
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/dashboard/user" element={<MainUserDashboard />} />
-    </Routes>
+    
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+
+        {/* General Authenticated Route */}
+        <Route
+          element={<ProtectedRoute isAllowed={!!user} redirectPath="/" />}
+        >
+          <Route path="/dashboard/user" element={<MainUserDashboard role={user.role}/>} />
+        </Route>
+
+        {/* Investor Only Route */}
+        <Route
+          path="/dashboard/investor"
+          element={
+            <ProtectedRoute
+              isAllowed={!!user && user.role === "investor"}
+              redirectPath="/"
+            >
+              <InvestorsDashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Entrepreneur Only Route */}
+        <Route
+          path="/dashboard/entrepreneur"
+          element={
+            <ProtectedRoute
+              isAllowed={!!user && user.role === "entrepreneur"}
+              redirectPath="/"
+            >
+              <EntrepreneurDashboard />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </>
   );
 }
 
--- frontend/src/dashboards/mainUserDashboard.jsx
+++ frontend/src/dashboards/mainUserDashboard.jsx
@@ -1,10 +1,8 @@
 import EntrepreneurDashboard from './entrepreneurs-dashboard/entrepreneursDashboard.jsx';
 import InvestorsDashboard from './investors-dashboard/investorsDashboard.jsx';
 
-function MainUserDashboard() {
+function MainUserDashboard( { role }) {
   // will be changed once we have authentication and user roles
-  const role = 'entrepreneur'
-
   if (role === 'investor') return <InvestorsDashboard />;
   if (role === 'entrepreneur') return <EntrepreneurDashboard />;
 }
